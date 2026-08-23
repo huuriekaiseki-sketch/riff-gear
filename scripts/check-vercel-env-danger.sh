aadd20c9f9cf7fddb2b112086f1634bb6c6fb64c
Masanori <>
2026-08-23 11:40:06 +0900

    fix: Vercelフックで空値の環境変数代入を検知

--- scripts/check-vercel-env-danger.sh
+++ scripts/check-vercel-env-danger.sh
@@ -19,7 +19,7 @@ split_segments() {
 # 「vercel env rm」「vercel env remove」がセグメント先頭の実行位置にあるか。
 # 環境変数代入、env、sudo、npx/pnpm dlx/yarn dlx経由、および実行ファイルの
 # 絶対・相対パス経由も、コマンド先頭の明示プレフィックスとして対象にする。
-VERCEL_ENV_RM_PATTERN='^[[:space:]]*(\([[:space:]]*)?([[:alnum:]_]+=[^[:space:]]+[[:space:]]+)*(env[[:space:]]+([[:alnum:]_]+=[^[:space:]]+[[:space:]]+)*)?(sudo[[:space:]]+)?((npx|pnpm[[:space:]]+dlx|yarn[[:space:]]+dlx)[[:space:]]+)?([[:alnum:]_./-]+/)?vercel[[:space:]]+env[[:space:]]+(rm|remove)([[:space:]]|$)'
+VERCEL_ENV_RM_PATTERN='^[[:space:]]*(\([[:space:]]*)?([[:alnum:]_]+=[^[:space:]]*[[:space:]]+)*(env[[:space:]]+([[:alnum:]_]+=[^[:space:]]*[[:space:]]+)*)?(sudo[[:space:]]+)?((npx|pnpm[[:space:]]+dlx|yarn[[:space:]]+dlx)[[:space:]]+)?([[:alnum:]_./-]+/)?vercel[[:space:]]+env[[:space:]]+(rm|remove)([[:space:]]|$)'
 
 INPUT="$(cat)"
 TOOL_NAME="$(printf '%s' "$INPUT" | jq -r '.tool_name // ""')"
